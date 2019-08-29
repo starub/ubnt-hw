e52736463310502eccfee78093cbb92750e39d88
oddone <4v8plzZgFYaoHdSeIcwl>
2019-08-29 20:52:07 +0300

    adjusted api

--- src/main/java/lv/starub/ubnt/application/API.java
+++ src/main/java/lv/starub/ubnt/application/API.java
@@ -7,9 +7,8 @@ import lv.starub.ubnt.domain.RedditPost;
 import lv.starub.ubnt.service.CacheService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
@@ -18,23 +17,23 @@ import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
-class APIFacade {
+class API {
 
     private static final String API_V_1 = "/api/v1/";
 
     private final CacheService cacheService;
 
-    @RequestMapping(value = {API_V_1 + "posts/{period}", API_V_1 + "posts"}, method = RequestMethod.GET)
+    @GetMapping(value = {API_V_1 + "posts/{period}", API_V_1 + "posts"})
     ResponseEntity<Collection<RedditPost>> posts(@PathVariable("period") Optional<PostPeriod> period) {
         return new ResponseEntity<>(cacheService.posts(period), HttpStatus.OK);
     }
 
-    @RequestMapping(value = {API_V_1 + "count/{type}/{period}", API_V_1 + "count/{type}"}, method = RequestMethod.GET)
+    @GetMapping(value = {API_V_1 + "count/{type}/{period}", API_V_1 + "count/{type}"})
     ResponseEntity<String> count(@PathVariable("type") PostType type, @PathVariable("period") Optional<PostPeriod> period) {
         return new ResponseEntity<>(cacheService.count(type, period), HttpStatus.OK);
     }
 
-    @RequestMapping(value = {API_V_1 + "top/{maxEntries}", API_V_1 + "top"}, method = RequestMethod.GET)
+    @GetMapping(value = {API_V_1 + "top/{maxEntries}", API_V_1 + "top"})
     ResponseEntity<Map<String, Long>> top(@PathVariable("maxEntries") Optional<Long> maxEntries) {
         return new ResponseEntity<>(cacheService.top(maxEntries), HttpStatus.OK);
     }
