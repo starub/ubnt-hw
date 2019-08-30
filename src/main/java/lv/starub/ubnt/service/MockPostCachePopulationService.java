50964bfde09cbcddceb63885b2e7e0aa7b42b3de
Stanislavs Rubens <Stanislavs.Rubens@rimibaltic.com>
2019-08-30 11:22:53 +0300

    tests, jacoco

--- src/main/java/lv/starub/ubnt/service/MockPostCachePopulationService.java
+++ src/main/java/lv/starub/ubnt/service/MockPostCachePopulationService.java
@@ -5,13 +5,12 @@ import com.hazelcast.core.IMap;
 import lombok.RequiredArgsConstructor;
 import lv.starub.ubnt.domain.Post;
 import lv.starub.ubnt.domain.PostPeriod;
+import lv.starub.ubnt.domain.PostType;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.time.Instant;
 
-@Component
 @RequiredArgsConstructor
 @Profile({"dev", "test"})
 public class MockPostCachePopulationService {
@@ -25,31 +24,36 @@ public class MockPostCachePopulationService {
 
         Post current = new Post();
         current.setTimestamp(now);
+        current.setSubreddit("current subreddit");
         current.setBody("current post");
+        current.setPostType(PostType.COMMENT);
 
         Post oneMinute = new Post();
         oneMinute.setTimestamp(now.minusSeconds(PostPeriod.ONE_MINUTE.getValue()));
+        oneMinute.setSubreddit("one minute subreddit");
         oneMinute.setBody("one minute post");
+        oneMinute.setPostType(PostType.SUBMISSION);
 
         Post fiveMinutes = new Post();
         fiveMinutes.setTimestamp(now.minusSeconds(PostPeriod.FIVE_MINUTES.getValue()));
+        fiveMinutes.setSubreddit("five minutes subreddit");
         fiveMinutes.setBody("five minutes post");
+        fiveMinutes.setPostType(PostType.COMMENT);
 
         Post oneHour = new Post();
         oneHour.setTimestamp(now.minusSeconds(PostPeriod.ONE_HOUR.getValue()));
+        oneHour.setSubreddit("one hour subreddit");
         oneHour.setBody("one hour post");
+        oneHour.setPostType(PostType.SUBMISSION);
 
         Post oneDay = new Post();
         oneDay.setTimestamp(now.minusSeconds(PostPeriod.ONE_DAY.getValue()));
+        oneDay.setSubreddit("one day subreddit");
         oneDay.setBody("one day post");
-
-        Post allTime = new Post();
-        allTime.setTimestamp(now.minusSeconds(PostPeriod.ALL_TIME.getValue()));
-        allTime.setBody("all time post");
+        oneDay.setPostType(PostType.COMMENT);
 
         IMap<Instant, Post> posts = hazelcastInstance.getMap("ALL_POSTS");
 
-        posts.put(allTime.getTimestamp(), allTime);
         posts.put(oneDay.getTimestamp(), oneDay);
         posts.put(oneHour.getTimestamp(), oneHour);
         posts.put(fiveMinutes.getTimestamp(), fiveMinutes);
---
+++ src/test/java/lv/starub/ubnt/service/DefaultPostServiceTest.java
@@ -0,0 +1,71 @@
+package lv.starub.ubnt.service;
+
+import lv.starub.ubnt.domain.Post;
+import lv.starub.ubnt.domain.PostPeriod;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class DefaultPostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @Test
+    @DisplayName("Test retrieval of all posts within one minute period")
+    void testPostsRetrievalOneMinuteInterval() {
+        Collection<Post> posts = postService.posts(Optional.of(PostPeriod.ONE_MINUTE));
+        assertThat(posts).hasSize(1);
+        assertThat(posts).extracting(Post::getSubreddit).containsExactly("current subreddit");
+    }
+
+    @Test
+    @DisplayName("Test retrieval of all posts within five minutes period")
+    void testPostsRetrievalFiveMinutesInterval() {
+        Collection<Post> posts = postService.posts(Optional.of(PostPeriod.FIVE_MINUTES));
+        assertThat(posts).hasSize(2);
+        assertThat(posts).extracting(Post::getSubreddit).containsExactly("one minute subreddit", "current subreddit");
+    }
+
+    @Test
+    @DisplayName("Test retrieval of all posts within one hour period")
+    void testPostsRetrievalOneHourInterval() {
+        Collection<Post> posts = postService.posts(Optional.of(PostPeriod.ONE_HOUR));
+        assertThat(posts).hasSize(3);
+        assertThat(posts).extracting(Post::getSubreddit).containsExactly("five minutes subreddit", "one minute subreddit", "current subreddit");
+    }
+
+    @Test
+    @DisplayName("Test retrieval of all posts within one day period")
+    void testPostsRetrievalOneDayInterval() {
+        Collection<Post> posts = postService.posts(Optional.of(PostPeriod.ONE_DAY));
+        assertThat(posts).hasSize(4);
+        assertThat(posts).extracting(Post::getSubreddit).containsExactly("one hour subreddit", "five minutes subreddit", "one minute subreddit", "current subreddit");
+    }
+
+//    @Test
+//    @DisplayName("Test retrieval of all posts within none/ALL_TIME period")
+//    void testAllPostsRetrieval() {
+//        Collection<Post> posts = postService.posts(Optional.empty());
+//        assertThat(posts).hasSize(5);
+//        assertThat(posts).extracting(Post::getSubreddit).containsExactly("one day subreddit","one hour subreddit","five minutes subreddit","one minute subreddit", "current subreddit");
+//
+//        posts = postService.posts(Optional.of(PostPeriod.ALL_TIME));
+//        assertThat(posts).hasSize(5);
+//        assertThat(posts).extracting(Post::getSubreddit).containsExactly("one day subreddit","one hour subreddit","five minutes subreddit","one minute subreddit", "current subreddit");
+//    }
+
+//    @Test
+//    @DisplayName("Test comments and submissions count within period")
+//    void testCommentAndSubmissionCount() {
+//        assertThat(postService.count(PostType.COMMENT,Optional.empty())).isEqualTo("\r\n3 COMMENTS\r\n");
+//        assertThat(postService.count(PostType.SUBMISSION,Optional.of(PostPeriod.ALL_TIME))).isEqualTo("\r\n2 SUBMISSIONS\r\n");
+//    }
+}
